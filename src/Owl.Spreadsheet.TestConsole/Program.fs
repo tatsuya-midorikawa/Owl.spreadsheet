56e4100d03de54f0eb3ebc7c1fea7fd27f556b52
midoliy <midorikawa@tech-leaves.com>
2021-03-25 22:42:19 +0900

    💡update

--- src/Owl.Spreadsheet.TestConsole/Program.fs
+++ src/Owl.Spreadsheet.TestConsole/Program.fs
@@ -1,6 +1,7 @@
 ﻿open Owl.Spreadsheet
 open Owl.Spreadsheet.Spreadsheet
 open Owl.Spreadsheet.Worksheet
+open Owl.Spreadsheet.ExcelFunction
 
 // ========================
 // Sample 1.
@@ -59,19 +60,22 @@ open Owl.Spreadsheet.Worksheet
 // ========================
 // Sample 4.
 
-let workbook = new_workbook_from "./template.xlsx"
-let cell = workbook |> get_sheet_at 1 |> get_cell
+//let workbook = new_workbook_from "./template.xlsx"
+//let cell = workbook |> get_sheet_at 1 |> get_cell
 
-// Set the number of lines in the header line as the offset value
-let offset = 1
-let today = datetime.Today
-for i in 1..10 do
-  let row = i + offset
-  cell(row, 1).set<string>(identifier.NewGuid().ToString())
-  cell(row, 2).set<string>($"midoliy {i:D3}")
-  cell(row, 3).set<datetime>(datetime(year=1989+i, month=9, day=13+i))
-  cell(row, 4).set_formula($"=DATEDIF({(row, 3).to_address()},TODAY(),\"Y\")")
-  // cell(row, 4).set<int>((integer.Parse(datetime.Today.ToString("yyyyMMdd")) - integer.Parse(cell(row, 3).get<datetime>().ToString("yyyyMMdd"))) / 10000)
+//// Set the number of lines in the header line as the offset value
+//let offset = 1
+//let today = datetime.Today
+//for i in 1..10 do
+//  let row = i + offset
+//  cell(row, 1).set<string>(identifier.NewGuid().ToString())
+//  cell(row, 2).set<string>($"midoliy {i:D3}")
+//  cell(row, 3).set<datetime>(datetime(year=1989+i, month=9, day=13+i))
+//  cell(row, 4).set_formula($"=DATEDIF({(row, 3).to_address()},TODAY(),\"Y\")")
+//  // cell(row, 4).set<int>((integer.Parse(datetime.Today.ToString("yyyyMMdd")) - integer.Parse(cell(row, 3).get<datetime>().ToString("yyyyMMdd"))) / 10000)
 
-workbook |> save_as "./output.xlsx" |> close
+//workbook |> save_as "./output.xlsx" |> close
 
+SUM [0; 1; 2;] |> printfn "%A"
+ABS 1.0 |> printfn "%f"
+POWER(2.0, 2.0) |> printfn "%f"
---
+++ src/Owl.Spreadsheet/Functions/ExcelFunction.fs
@@ -0,0 +1,37 @@
+﻿namespace Owl.Spreadsheet
+
+open System.Linq
+
+module ExcelFunction =
+  let inline IF(expression, when_true, when_false) =
+    if expression then when_true else when_false
+  let inline AND(args: bool seq) = args.All(fun arg -> arg)
+  let inline OR(args: bool seq) = args.Any(fun arg -> arg)
+  let inline TODAY() = datetime.Today
+  let inline NOW() = datetime.Now
+  let inline MAX(args: ^T seq) = Seq.max args
+  let inline MIN(args: ^T seq) = Seq.min args
+  let inline SMALL(args: ^T seq, rank: int) = Seq.sort args |> Seq.item rank
+  let inline SUM(args: ^T seq) = Seq.sum args
+  let inline AVERAGE(args: ^T seq) = Seq.average args
+  // TODO
+  let inline COUNT() = raise(exn "")
+  let inline FLOOR(number) = floor number
+  let inline ABS(number) = abs number
+  let inline SIN(number) = sin number
+  let inline COS(number) = cos number
+  let inline TAN(number) = tan number
+  let inline SINH(number) = sinh number
+  let inline COSH(number) = cosh number
+  let inline TANH(number) = tanh number
+  let inline MOD(number: ^T, divisor: ^T) = number % divisor
+  let inline POWER(number: ^T, divisor: ^T) = number ** divisor
+  let inline PRODUCT(number1: ^T, number2: ^T) = number1 * number2
+  // TODO
+  let inline LOOKUP() = raise(exn "")
+  // TODO
+  let inline VLOOKUP() = raise(exn "")
+  // TODO
+  let inline HLOOKUP() = raise(exn "")
+  // TODO
+  let inline XLOOKUP() = raise(exn "")
