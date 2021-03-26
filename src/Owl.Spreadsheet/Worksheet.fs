3fe21d3118d818044b135cffeecea45016b92bfe
midoliy <midorikawa@tech-leaves.com>
2021-03-26 17:38:34 +0900

    💡update

--- src/Owl.Spreadsheet/Worksheet.fs
+++ src/Owl.Spreadsheet/Worksheet.fs
@@ -7,17 +7,20 @@ module Worksheet =
   let public get_cell (sheet: IXLWorksheet) ((row, colmun): int * int) =
     Cell(sheet.Cell(row, colmun))
 
-  let public get_cell_at (sheet: IXLWorksheet) (address: Address) =
-    Cell(sheet.Cell(address.row, address.column))
+  let public get_cell_at (sheet: IXLWorksheet) (address: string) =
+    Cell(sheet.Cell address)
 
-  let public get_cells (sheet: IXLWorksheet) (from': int * int) (to':  int * int)  =
+  let public get_cells (sheet: IXLWorksheet) (from': int * int) (to': int * int) =
     let address = $"%s{from'.to_address()}:%s{to'.to_address()}"
     Cells(sheet.Cells address)
     
-  let public get_cells_at (sheet: IXLWorksheet) (from': Address) (to': Address)  =
-    let address = $"%s{from'.to_tuple().to_address()}:%s{to'.to_tuple().to_address()}"
-    Cells(sheet.Cells address)
+  let public get_cells_at (sheet: IXLWorksheet) (range: string)  =
+    Cells(sheet.Cells range)
 
-  let public get_range (sheet: IXLWorksheet) (from': Address) (to': Address) =
-    let address = $"%s{from'.to_tuple().to_address()}:%s{to'.to_tuple().to_address()}"
+  let public get_range (sheet: IXLWorksheet) (from': int * int) (to': int * int)=
+    let address = $"%s{from'.to_address()}:%s{to'.to_address()}"
     Owl.Spreadsheet.Range(sheet.Range address)
+
+  let public get_range_at (sheet: IXLWorksheet) (range: string)=
+    Owl.Spreadsheet.Range(sheet.Range range)
+
