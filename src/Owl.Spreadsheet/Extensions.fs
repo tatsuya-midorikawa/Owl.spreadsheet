72363cc174f04724ed04b9a2e4eb4a60d370bd79
midoliy <midorikawa@tech-leaves.com>
2021-04-09 14:55:18 +0900

    💡update

--- src/Owl.Spreadsheet/Extensions.fs
+++ src/Owl.Spreadsheet/Extensions.fs
@@ -1,7 +1,9 @@
 ﻿namespace Owl.Spreadsheet
 
+open System
 open System.Collections.Generic
 open System.Collections.Concurrent
+open System.Linq
 open System.Text
 open System.Runtime.CompilerServices
 
@@ -10,6 +12,38 @@ module internal SpreadsheetHelper =
   [<Literal>]
   let column_name_table = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   let table_size = column_name_table.Length
+
+  [<Literal>]
+  let alphabets = "ZABCDEFGHIJKLMNOPQRSTUVWXYzabcdefghijklmnopqrstuvwxy"
+  [<Literal>]
+  let upper_alphabets = "ZABCDEFGHIJKLMNOPQRSTUVWXY"
+  [<Literal>]
+  let lower_alphabets = "zabcdefghijklmnopqrstuvwxy"
+
+  let is_alphabet (c: char) =
+    Seq.exists (fun alphabet -> alphabet = c) alphabets
+  let is_upper_alphabet (c: char) =
+    Seq.exists (fun alphabet -> alphabet = c) upper_alphabets
+  let is_lower_alphabet (c: char) =
+    Seq.exists (fun alphabet -> alphabet = c) lower_alphabets
+
+  let are_consists_only_of_alphabets (target: string) =
+    target.All(fun c -> is_alphabet c)
+
+  let rec column_number_rec (name: ReadOnlySpan<char>) (acc: int) =
+    let digit = name.Length - 1
+    if 0 <= digit then
+      let n = (int name.[0]) - 64
+      let sum = acc + n * (26.0 ** float digit |> int)
+      column_number_rec (name.Slice(1)) sum
+    else
+      acc
+
+  let column_number (name: string) =
+    if name |> String.IsNullOrEmpty || not (are_consists_only_of_alphabets name) then
+      raise (exn $"invalid column name: {name}")
+    else
+      column_number_rec (name.ToUpper().AsSpan()) 0
 
 [<Extension>]
 type SpreadsheetHelper =
@@ -52,3 +86,9 @@ type SpreadsheetHelper =
   [<Extension>]
   static member public to_address((row, column): int * int) =
     $"%s{column.to_column_name()}%d{row}"
+
+  /// <summary>
+  /// Spreadsheetの列名を数値に変換する
+  /// </summary>
+  static member public to_column_number(name: string) =
+    SpreadsheetHelper.column_number name
