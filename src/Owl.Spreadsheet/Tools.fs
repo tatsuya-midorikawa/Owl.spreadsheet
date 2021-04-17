5116c5bcdf528a80a23715aeb65af12cd18f65bb
midoliy <midorikawa@tech-leaves.com>
2021-04-17 10:07:35 +0900

    💡update

--- src/Owl.Spreadsheet/Tools.fs
+++ src/Owl.Spreadsheet/Tools.fs
@@ -1,6 +1,5 @@
-namespace Owl.Spreadsheet
+﻿namespace Owl.Spreadsheet
 
-open System.Linq
 open ClosedXML.Excel
 
 module Tools =
@@ -11,6 +10,7 @@ module Tools =
 
   let load filepath = XlWorkbook.open_workbook filepath
   let create name = XlWorkbook.new_workbook_with name
+  let create_from template = XlWorkbook.new_workbook_from template
   let workbook (sheet: XlWorksheet) = sheet.raw.Workbook
 
   let fst (workbook: XLWorkbook) = workbook.Worksheet(1) |> XlWorksheet
@@ -30,4 +30,17 @@ module Tools =
   let save_and_close (sheet: XlWorksheet) = sheet |> workbook |> XlWorkbook.save_and_close |> ignore
   let close (sheet: XlWorksheet) = sheet |> workbook |> XlWorkbook.close |> ignore
 
+  let cell (row: int, column: int) (sheet: XlWorksheet) = XlWorksheet.get_cell sheet (row, column)
+  let cells (range: string) (sheet: XlWorksheet) = XlWorksheet.get_cells_at sheet range
+  let range (range: string) (sheet: XlWorksheet) = XlWorksheet.get_range_at sheet range
   
+  let row (row: int) (sheet: XlWorksheet) = XlWorksheet.get_row sheet row
+  let rows (first': int, last': int) (sheet: XlWorksheet) = XlWorksheet.get_rows sheet (first', last')
+  let rows_for (rows: string) (sheet: XlWorksheet) = XlWorksheet.get_rows_at sheet rows
+  let all_rows (sheet: XlWorksheet) = XlWorksheet.get_all_rows sheet
+
+  let column (column: int) (sheet: XlWorksheet) = XlWorksheet.get_column sheet column
+  let column_at (column: string) (sheet: XlWorksheet) = XlWorksheet.get_column_at sheet column
+  let columns (from': int, to': int) (sheet: XlWorksheet) = XlWorksheet.get_columns sheet (from', to')
+  let columns_for (columns: string) (sheet: XlWorksheet) = XlWorksheet.get_columns_at sheet columns
+  let all_columns (sheet: XlWorksheet) = XlWorksheet.get_all_columns sheet
